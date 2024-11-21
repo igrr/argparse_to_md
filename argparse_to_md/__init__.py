92d2fd57e123f03142f016cad9829643889c5fd3
Ivan Grokhotkov <ivan@espressif.com>
2024-11-21 16:36:11 +0100

    ci: add pre-commit job, upgrade setup-python action

--- argparse_to_md/__init__.py
+++ argparse_to_md/__init__.py
@@ -1,4 +1,9 @@
-from ._version import __version__
+try:
+    from ._version import __version__
+except ImportError:
+    # This happens when the package is not installed, e.g. when running pre-commit in CI
+    __version__ = "0.0.0"
+
 from .loader import FunctionLoader
 from .markdown_processor import process_markdown
 
